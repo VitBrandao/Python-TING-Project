1398908ba77352e528669acf82f8fb7279ae9165
vitor <vitorsbsilva@gmail.com>
2022-09-14 11:24:03 -0300

    feat: process function in file_process.py

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação."""
+    imported_file = txt_importer(path_file)
+    instance_len = len(instance)
+
+    for item in range(instance_len):
+        if instance.search(item)["nome_do_arquivo"] == path_file:
+            return None
+
+    final_content = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": imported_file.__len__(),
+        "linhas_do_arquivo": imported_file
+    }
+
+    instance.enqueue(final_content)
+
+    return sys.stdout.write(str(final_content))
 
 
 def remove(instance):
