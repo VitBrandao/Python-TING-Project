9098fbd6c2fa1c40873161b813fba378071aceee
vitor <vitorsbsilva@gmail.com>
2022-09-14 11:52:24 -0300

    feat: function exists_word in word_search.py

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,28 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    data = instance.data
+
+    final_object = []
+    count_lines = []
+
+    for file in data:
+        indexed_list = enumerate(file["linhas_do_arquivo"])
+        # print(indexed_list)
+        for index, line in indexed_list:
+            if word.lower() in line.lower():
+                count_lines.append({"linha": index + 1})
+
+        # print(count_lines)
+
+        final_object.append({
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": count_lines,
+            "palavra": word,
+        })
+
+    if count_lines.__len__() == 0:
+        return count_lines
+
+    return final_object
 
 
 def search_by_word(word, instance):
