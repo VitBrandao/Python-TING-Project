a7ca84c5bd6f593658291d5710a36f77cfb00826
vitor <vitorsbsilva@gmail.com>
2022-09-15 15:48:05 -0300

    feat: (req.6) Finishing project.

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="ting",
-    description="Projeto ting",
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest"],
-    packages=["ting_file_management", "ting_word_searches"],
-)
